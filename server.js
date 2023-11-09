b3818c711b8ecf6fb253b26d89702008047fcc13
ljodice <>
2023-11-08 22:56:44 -0500

    -i Axios; -r fetch imp for MBTA API

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var fs = require("fs");
 var express = require("express");
+const axios = require("axios");
 // var mongoose = require("mongoose");
-const stops = "mbta-json/tstops.json";
+// var customfetch = require("./modules/customfetch");
+const tstops = "mbta-json/tstops.json";
 var app = express();
 const trainStops = [];
 app.set("json spaces", 2);
@@ -28,25 +30,39 @@ var server = app.listen(3000);
 app.use(express.static("public"));
 app.use(express.json());
 
-getstops();
 //encorporate this:
 //https://developer.here.com/develop/javascript-api
 
+// FUNCTION readjson() IS DEPRICATED FOR THE TIME BEING @11/1/2023
+//REASON:Feels like each of the endpoints read in a file locally and use it in different ways
+
+// function readjson(filename){ // this needs to be a string/filepath
+//   fs.readFile(filename, "utf8", (err, data) => {
+//     if (err) {
+//       console.error("Error reading the file:", err);
+//       res.status(500).send("Internal Server Error");
+//     } else {
+//       try {
+//         const json = JSON.parse(data);
+//         return json.current; // Send the JSON data as a response
+//       } catch (parseError) {
+//         console.error("Error parsing JSON:", parseError);
+//         res.status(500).send("Internal Server Error");
+//       }
+//     }
+//   });
+// }
+
 function getstops() {
   if (trainStops.length === 0) {
-    console.log("nothing");
-    fs.readFile(stops, "utf8", (err, data) => {
+    //If the cached array is empty then read in the values from json
+    fs.readFile(tstops, "utf8", (err, data) => {
       if (err) {
         console.error(err);
         res.status(500).send("Error reading the JSON file");
       } else {
         const jsonData = JSON.parse(data);
-        console.log(jsonData["data"][0]);
         if (trainStops.length === 0) {
-          console.log("yep");
-          console.log(jsonData["data"].length);
-          console.log(jsonData["data"][1]["id"]);
-
           for (var i = 0; i < jsonData["data"].length; i++) {
             var idx = jsonData["data"][i];
             var obj = {
@@ -69,13 +85,13 @@ function getstops() {
       }
     });
   } else {
-    console.log(trainStops.length);
+    //the values are already loaded into the array
+    console.log(trainStops.length); // length printed for error checking
     return trainStops;
   }
 }
 
 app.post("/coords", (req, res) => {
-  console.log("got post");
   console.log(req.body);
   const data = req.body;
   res.json({ status: "Success", long: data.long, lat: data.lat });
@@ -128,7 +144,7 @@ app.get("/stops", (req, res) => {
 });
 
 app.get("/trains", (req, res) => {
-  fs.readFile(stops, "utf8", (err, data) => {
+  fs.readFile(tstops, "utf8", (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).send("Error reading the JSON file");
@@ -136,8 +152,6 @@ app.get("/trains", (req, res) => {
       const jsonData = JSON.parse(data);
       console.log(jsonData["data"][0]);
       if (trainStops.length === 0) {
-        console.log("yep");
-        console.log(jsonData["data"].length);
         console.log(jsonData["data"][1]["id"]);
 
         for (var i = 0; i < jsonData["data"].length; i++) {
@@ -166,45 +180,49 @@ app.get("/trains", (req, res) => {
 });
 
 app.get("/trains/:lineName", (req, res) => {
-  var stops = [];
-  fs.readFile(stops, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error reading the JSON file");
-    } else {
-      const jsonData = JSON.parse(data);
-      console.log(jsonData["data"][0]);
-      if (trainStops.length === 0) {
-        console.log("yep");
-        console.log(jsonData["data"].length);
-        console.log(jsonData["data"][1]["id"]);
-
-        for (var i = 0; i < jsonData["data"].length; i++) {
-          var idx = jsonData["data"][i];
-          var obj = {
-            lineName: idx["id"],
-            lineColor: idx["attributes"]["color"],
-            trackEnds: [
-              {
-                name: idx["attributes"]["direction_destinations"][0],
-                direction: idx["attributes"]["direction_names"][0],
-              },
-              {
-                name: idx["attributes"]["direction_destinations"][1],
-                direction: idx["attributes"]["direction_names"][1],
-              },
-            ],
-          };
-          stops.push(obj);
-          // if (obj.lineName === req.params.lineName) {
-          //   console.log("same");
-          //   res.json(obj);
-          //   return;
-          // }
-        }
-      }
+  var stops = getstops();
+  for (var i = 0; i < stops.length; i++) {
+    if (stops[i]["lineName"] === req.params.lineName) {
+      res.json(stops[i]);
+      return; // Add 'return' to exit the loop when a match is found
     }
-  });
+  }
+  // If no match is found, you can send an appropriate response, e.g., 404 Not Found
+  res.status(404).send("Train line not found");
+});
 
-  res.json(req.params);
+//gets the routes from given line
+app.get("/mbta-stops/:route_id", async (req, res) => {
+  // Get the train route ID from the request parameter
+  const routeId = req.params.route_id;
+  // Make a request to the MBTA API to get all of the stops on the route
+  try {
+    const response = await axios.get(
+      `https://api-v3.mbta.com/stops?filter[route]=${routeId}`
+    );
+    const stopsJson = response.data;
+
+    // Send the stops back to the client
+    res.send(stopsJson);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("An error occurred while fetching data.");
+  } // Send the stops back to the client
+});
+
+//get the times of the trains comming into a given stop
+app.get("/mbta-train-times/:stop_name", async (req, res) => {
+  // Get the train stop name from the request parameter
+  const stopName = req.params.stop_name;
+  try {
+    const response = await axios.get(
+      "https://api-v3.mbta.com/predictions?filter[stop]=place-${stopName}"
+    );
+    const stopJson = response.data;
+
+    res.send(stopJson);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("An error occurred while fetching data.");
+  }
 });
