2593db2ed0c23b7dd930d471eb666a97c7168b93
ljodice <>
2023-11-09 18:50:54 -0500

    fixing coords and local weather for remote use

--- server.js
+++ server.js
@@ -91,32 +91,31 @@ function getstops() {
   }
 }
 
-app.post("/coords", (req, res) => {
-  console.log(req.body);
-  const data = req.body;
-  res.json({ status: "Success", long: data.long, lat: data.lat });
-});
-
 app.get("/weather", (req, res) => {
   var getreq = fetch("weatherEX.json");
   const json = getreq.json();
   console.log(json);
 });
 
-//LOCAL TESTING ROUTE
+app.post("/coords", (req, res) => {
+  console.log(req.body);
+  const data = req.body;
+  res.json({ status: "Success", long: data.long, lat: data.lat });
+});
+
 app.get("/weatherlocal", (req, res) => {
   fs.readFile("weatherEX.json", "utf8", (err, data) => {
     if (err) {
       console.error("Error reading the file:", err);
-      res.status(500).send("Internal Server Error");
+      res.status(500).json({ error: "Internal Server Error" });
     } else {
       try {
         const json = JSON.parse(data);
-        var currweather = json.current;
-        res.json(currweather); // Send the JSON data as a response
+        const currWeather = json.current;
+        res.json(currWeather);
       } catch (parseError) {
         console.error("Error parsing JSON:", parseError);
-        res.status(500).send("Internal Server Error");
+        res.status(500).json({ error: "Internal Server Error" });
       }
     }
   });
